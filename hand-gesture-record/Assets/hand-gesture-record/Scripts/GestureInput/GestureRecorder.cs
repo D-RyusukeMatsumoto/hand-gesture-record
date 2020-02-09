95e91a915f078a20a96b8234d5ac3a44bfe4e9d9
D-RyusukeMatsumoto <matsumoto@dedication.co.jp>
2020-02-09 16:52:24 +0900

    ジェスチャを記録する部分コンポーネントの作成

---
+++ hand-gesture-record/Assets/hand-gesture-record/Scripts/GestureInput/GestureRecorder.cs
@@ -0,0 +1,73 @@
+﻿using HandGestureRecord.Common;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+namespace HandGestureRecord.GestureInput
+{
+    /// <summary>
+    /// ジェスチャーレコードクラス.
+    /// </summary>
+    public class GestureRecorder : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        GestureManager manager;
+
+        [SerializeField] KeyCode keyCode = KeyCode.Space;
+        
+        void Start()
+        {
+            manager = ManagerProvider.GetRuntimeManager<GestureManager>();
+        }
+
+        
+        void FixedUpdate()
+        {
+        }
+
+
+
+        /// <summary>
+        /// レコード部分インスペクタ拡張.
+        /// </summary>
+        [CustomEditor(typeof(GestureRecorder))]
+        public class GestureRecorderInspector : Editor
+        {
+            bool isRecoredStarted = false;
+            
+            public override void OnInspectorGUI()
+            {
+                var component = target as GestureRecorder;
+
+                component.keyCode = (KeyCode) EditorGUILayout.EnumPopup("録画操作キーコード", component.keyCode);
+            
+                
+                if (isRecoredStarted)
+                {
+                    if (GUILayout.Button("録画終了"))
+                    {
+                        // パスを指定するウィンドウを表示してScriptableObjectで保存.
+                        isRecoredStarted = false;
+                    }
+                }
+                else
+                {
+                    if (GUILayout.Button("録画開始") && EditorApplication.isPlaying)
+                    {
+                        isRecoredStarted = true;
+                    }
+                }
+            }
+            
+            
+            
+            
+        }
+
+
+#endif
+    }
+}
