93ee6f3a74479597d75fa00fbfbaa32c1d62e3ba
D-RyusukeMatsumoto <matsumoto@dedication.co.jp>
2020-02-09 20:12:00 +0900

    記録したジェスチャデータ

---
+++ hand-gesture-record/Assets/hand-gesture-record/Scripts/GestureInput/GestureRecordData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace HandGestureRecord.GestureInput
+{
+    /// <summary>
+    /// 記録したジェスチャーデータ.
+    /// </summary>
+    public class GestureRecordData : ScriptableObject
+    {
+        [SerializeField] HandDataBase.FingerStraightRatioInfo fingerStraightRatioInfo;
+        public HandDataBase.FingerStraightRatioInfo FingerStraightRatio => fingerStraightRatioInfo;
+
+        public GestureRecordData(
+            HandDataBase.FingerStraightRatioInfo argFingerStraightRatioInfo)
+        {
+            fingerStraightRatioInfo = argFingerStraightRatioInfo;
+        }
+
+    }
+}
