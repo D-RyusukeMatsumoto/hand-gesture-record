f07be0611688eada013709db093a5dbfd7f068dc
D-RyusukeMatsumoto <matsumoto@dedication.co.jp>
2020-02-12 03:58:58 +0900

    アプリの初期化処理でプレハブを生成するようにした

--- hand-gesture-record/Assets/hand-gesture-record/Scripts/Common/ApplicationInitialize.cs
+++ hand-gesture-record/Assets/hand-gesture-record/Scripts/Common/ApplicationInitialize.cs
@@ -1,7 +1,19 @@
-﻿namespace HandGestureRecord.Common
+﻿using UnityEngine;
+
+namespace HandGestureRecord.Common
 {
-    public class ApplicationInitialize
+    /// <summary>
+    /// ゲームの開始時,Awakeより前に一度だけ呼び出される処理.
+    /// </summary>
+    public static class ApplicationInitialize
     {
-        
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        static void InitializeApp()
+        {
+            // ここでGameMangerを生成する.
+            var go = GameObject.Instantiate(Resources.Load("Prefabs/ManagerProvider")) as GameObject;
+            go.name = go.name.Replace("(Clone)", "");
+        }
     }
 }
