b510e8ea80a0bfddc43dc5950f8f1a89f8bc60b6
D-RyusukeMatsumoto <matsumoto@dedication.co.jp>
2020-04-12 17:04:33 +0900

    UnityPackageに出力しないものをフォルダ分けして配置、ScriptableObjectCreateMenuを追加

---
+++ hand-gesture-record/Assets/hand-gesture-record/NotPackaging/Scripts/Editor/ScriptableObjectCreateMenu.cs
@@ -0,0 +1,47 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace NoPackaging
+{
+    public static class ScriptableObjectCreateMenu
+    {
+
+        const string MenuText = "Assets/Create/ScriptableObject";
+        const string PanelTitle = "CreateScriptableAsset";
+        const string DefaultFileName = "ScriptableAsset";
+        const string Extension = "asset";
+
+
+        // コンテキストメニュー.
+        [MenuItem(MenuText, false, 0)]
+        static void CreateScriptableObject()
+        {
+            var script = Selection.activeObject as MonoScript;
+            string filePath = EditorUtility.SaveFilePanelInProject(PanelTitle, DefaultFileName, Extension, "");
+            OutputFile(script.GetClass(), filePath);            
+        }
+
+
+        // ファイルの生成.
+        static void OutputFile(
+            System.Type type, 
+            string outputPath)
+        {
+            ScriptableObject scriptableObject = ScriptableObject.CreateInstance(type);
+            AssetDatabase.CreateAsset(scriptableObject, outputPath);
+            AssetDatabase.Refresh();
+            ProjectWindowUtil.ShowCreatedAsset(scriptableObject);
+        }
+
+
+        // 選択しているスクリプトがScriptableObjectか否か判定.
+        [MenuItem(MenuText, true)]
+        static bool ValidateCreateAsset()
+        {
+            var script = Selection.activeObject as MonoScript;
+            if (script == null) return false;
+            return script.GetClass().IsSubclassOf(typeof(ScriptableObject));
+        }
+
+    }
+}
